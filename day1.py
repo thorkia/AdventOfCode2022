12eecbefd53bd02c1fb2ecfc867accc50fbf13ad
Robert Maissan <robert.maissan@gmail.com>
2022-12-01 22:03:07 -0500

    Day 1 Solutions

---
+++ day1.py
@@ -0,0 +1,24 @@
+
+filename = '.\\input\day1.txt'
+
+with open(filename) as f:
+    lines = [x for x in f.readlines()]
+    f.close()
+
+elves =  [ ]
+currentElf = 0
+elves.append(0)
+
+for line in lines:
+    if line.strip() == "":
+        currentElf+=1
+        elves.append(0)
+    else:
+        elves[currentElf] += int(line.strip())
+#Part 1
+mostCalories = max(elves)
+print(mostCalories)
+
+#Part 2
+elves.sort(reverse=True)
+print(sum(elves[:3]))
