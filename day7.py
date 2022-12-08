39837ad41778ed819cbc2eaab763fad1ed47b792
thorkia <thorkia@gmail.com>
2022-12-07 23:14:45 -0500

    Day 7 - still not correct.  I am off by exactly 58554 for part one.
    The problem is in this directory.  I know what issue is.
    
    When generating the directory size, I should only count the files under the directory not each directories total.
    
    My logic breaks when you have multiple levels of empy logic
    
    ./ldqc/jclb/pbb/zmflq/lcgv/rfzrwc/wqmdw,117108
    ./ldqc/jclb/pbb/zmflq/lcgv/rfzrwc/wqmdw/zmrdf,58554
    ./ldqc/jclb/pbb/zmflq/lcgv/rfzrwc/wqmdw/zmrdf/qqgw,58554

--- day7.py
+++ day7.py
@@ -1,21 +1,32 @@
-filename = '.\\input\day7test.txt'
-#filename = '.\\input\day7.txt'
+#filename = '.\\input\day7test.txt'
+filename = '.\\input\day7.txt'
             
 class Directory:        
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, parent : None) -> None:
         self.path = path
         self.files = [ ]
-        self.subdir:list[Directory]
+        self.subdir:list[Directory] = [ ]
+        self.parent = parent
 
     def getSizes(self) -> dict[str,int]:
         sizes = { }
         sizes[self.path] = sum([f[1] for f in self.files])
 
-        for dir in self.subdir:            
-            for k,v in dir.getSizes():
+        for dir in self.subdir:
+            subSizes = dir.getSizes()
+            sizes[self.path]+= sum([s for s in subSizes.values()]) # the current folder also includes all child folder values
+            for k,v in subSizes.items():
                 sizes[k] = v
 
         return sizes
+    
+    def getFileSizes(self) -> int:
+        size = 0
+        size+=sum([f[1] for f in self.files])
+        for dir in self.subdir:
+            size+=dir.getFileSizes()
+        
+        return size
     
 
 
@@ -23,41 +34,43 @@ with open(filename) as f:
     lines = [x for x in f.readlines()]
     f.close()
 
-dirs = { }
-paths = [ "./",  ]
-dir = { "path": "".join(paths), "files": [ ] }
 
-for line in lines[1:]: #skip the first cd since we've created it to start
+rootDirectory = None
+currentDirectory = None
+
+for line in lines: #skip the first cd since we've created it to start
     if line.startswith("$ cd"):
-        if dir["path"] not in dirs.keys():
-            dirs[dir["path"]] = dir
-        
-        if (line.split(" ")[-1].strip()==".."):
-            paths.pop()
+        if line.split()[-1].strip() == "/":            
+            rootDirectory = Directory(".", None)
+            currentDirectory = rootDirectory
+        elif line.split(" ")[-1].strip()=="..":
+            currentDirectory = currentDirectory.parent
         else:
-            paths.append(line.split()[-1].strip() + "/")
+            newDir = Directory(currentDirectory.path + "/" + line.split()[-1].strip(), currentDirectory)
+            currentDirectory.subdir.append(newDir)
+            currentDirectory = newDir
 
-        dir = { "path": "".join(paths), "files": [ ] }
-    
-    if line[0].isdigit():
-        dir["files"].append( (line.split()[1], int(line.split()[0])))
+    elif line[0].isdigit():
+        currentDirectory.files.append( (line.split()[1], int(line.split()[0])) )        
 
-#add the last directory
-dirs[dir["path"]] = dir
+sizes = rootDirectory.getSizes()
 
+print(sizes.keys())
 
+print([ k for k,v in sizes.items() if v<=100000 ])
+print( sum([ s for s in sizes.values() if s<=100000 ]) )
 
-for d in dirs.values():
-    size = sum([i[1] for i in d["files"]])
-    print(d["path"] + ":" + str(size))
+#Day 2
+spaceAvailable = 70000000
+targetSpace = 30000000
+spaceUsed = rootDirectory.getFileSizes()
 
+freeSpace = spaceAvailable - spaceUsed
 
+neededSpace = targetSpace - freeSpace
 
-fileLists = [d["files"] for d in dirs.values()]
-fileTotals = [ sum([fd[1] for fd in f]) for f in fileLists]
+dirsSmaller = [ (k,v) for k,v in sizes.items() if v>=neededSpace ]
 
-allTogether = sum( [ s for s in fileTotals if s<=100000] )
-
-print(allTogether)
-
+small = min( [s[1] for s in dirsSmaller])
+print(small)
 #print(dirs.keys())
